6331e179fbe699faaeea61bb83a86f69c586cefd
xsSplater - СанСаныч <xssplater@gmail.com>
2026-09-04 11:42:33 +0300

    3_0_0

--- Servo-Modquisitor-2/config.go
+++ Servo-Modquisitor-2/config.go
@@ -11,7 +11,7 @@ import (
 // ───────────── Программа ─────────────────────────────────────────
 const (
 	AppName         = "Servo-Modquisitor-2"
-	AppVersion      = "2.9.77"
+	AppVersion      = "3.0.0"
 	AppID           = "com.xssplater.servo-modquisitor"
 	AppIcon         = "assets/icon.png"
 	СonfigFolderSMQ = "Servo-Modquisitor"
